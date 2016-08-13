a672f113d65fac1ad4b5365d2c517fb6f0b40c04
dongwin22 <dongwin21@126.com>
2016-08-13 09:21:46 +0800

    这是第二页里面的lv和rv的解析

---
+++ app/src/main/java/com/example/dllo/keepproject/model/bean/FindFmrecommendBean.java
@@ -0,0 +1,192 @@
+package com.example.dllo.keepproject.model.bean;
+
+import java.util.List;
+
+/**
+ * Created by dllo on 16/8/12.
+ */
+public class FindFmrecommendBean {
+
+
+    /**
+     * ok : true
+     * data : [{"user":{"_id":"54bf30dd9eb5cd2c4c90be46","username":"Keep","avatar":"http://static1.gotokeep.com/avatar/2016/01/04/15/123ef56eda66391ec83a8a0290fa813bc248c3c8.jpg","relation":0},"entries":[{"_id":"57a83380a1987a1f705f674f","photo":"http://static1.gotokeep.com/misc/2016/08/09/10/559b4f7ce6c00000.jpg","type":"article"},{"_id":"579e9a3023cf904405062a42","photo":"http://static1.gotokeep.com/picture/2016/08/01/08/732ed62b28b5c6d4d62faaacdd5c3dfe606135ed_612x612.jpg","type":"direct"},{"_id":"5797504b7e7db1261d8d0864","photo":"http://static1.gotokeep.com/misc/2016/07/26/19/5589bfb3b3400000.jpg","type":"article"}],"source":"Keep 推荐","reason":null},{"user":{"_id":"573c400fdfdb55eb30eaf3db","username":"Keep_商城","avatar":"http://static1.gotokeep.com/avatar/2016/05/19/15/6b019080ca367881feaac8e5fe06c0be2bf58dec.jpg","relation":0},"entries":[{"_id":"57a9b6ad414b6ed70a176394","photo":"http://static1.gotokeep.com/misc/2016/08/09/18/559bbb690dc00000.jpg","type":"article"},{"_id":"57a07fac7fcccc8c21d2e6eb","photo":"http://static1.gotokeep.com/misc/2016/08/02/19/5592bc0be9400000.jpg","type":"article"},{"_id":"5799fbccc9f1115a4dd0f3b1","photo":"http://static1.gotokeep.com/misc/2016/07/28/20/558c5f38a6800000.jpg","type":"article"}],"source":"Keep 推荐","reason":null},{"user":{"_id":"56178e241779a99315da9373","username":"Keep_精选","avatar":"http://static1.gotokeep.com/avatar/2016/00/04/15/1d5cd58f31196bc4690f031ed469613149eb8196.jpg","relation":0},"entries":[{"_id":"57ad1ef1812abc461f6f1980","photo":"http://static1.gotokeep.com/misc/2016/08/12/08/559f0ffc36c00000.jpg","type":"article"},{"_id":"57a918cea1987a1f705f6e5d","photo":"http://static1.gotokeep.com/misc/2016/08/09/12/559b64c0e2c00000.jpg","type":"article"},{"_id":"57a6a2baa1987a1f705f59bf","photo":"https://staticssl.gotokeep.com/misc/2016/08/07/12/5598ca9201000000.jpg","type":"article"}],"source":"Keep 推荐","reason":null},{"user":{"_id":"549cd5697e7ed6ee5c94ba4b","username":"Keep_技术分享","avatar":"http://static1.gotokeep.com/avatar/2016/00/04/15/f32e979b6e5c6432743b66366ab3e0c790a68f66.jpg","relation":0},"entries":[{"_id":"57ab0880c97c35034511e508","photo":"http://static1.gotokeep.com/picture/2016/08/10/18/c541d88fa10527445106533b2e342987273ebba1_612x612.jpg","type":"direct"},{"_id":"57a7d02ba1987a1f705f61cd","photo":"http://static1.gotokeep.com/misc/2016/08/08/12/559a170bc7400000.jpg","type":"article"},{"_id":"57a531f59bd512b71462f9a0","photo":"http://static1.gotokeep.com/misc/2016/08/06/08/55974c48bb800000.jpg","type":"article"}],"source":"Keep 推荐","reason":null},{"user":{"_id":"55ac969edceb3b0fdf13bcfd","username":"Keep_吃惑","avatar":"http://static1.gotokeep.com/avatar/2016/02/17/15/19870d900ac396b892567b96f3398f99c0042d13.jpg","relation":0},"entries":[{"_id":"57aaf13bf933c4607d5eab3f","photo":"https://staticssl.gotokeep.com/misc/2016/08/11/12/559df1ba53000000.jpg","type":"article"},{"_id":"57a98332414b6ed70a176184","photo":"https://staticssl.gotokeep.com/misc/2016/08/10/11/559c9e885b400000.jpg","type":"article"},{"_id":"57a44f779bd512b71462f43f","photo":"http://static1.gotokeep.com/misc/2016/08/05/19/55969db322000000.jpg","type":"article"}],"source":"Keep 推荐","reason":null},{"user":{"_id":"5588c6cbb7d3f7ee66a50bb0","username":"Keep_你是教练","avatar":"http://static1.gotokeep.com/avatar/2016/00/04/15/ef739ee59e97c9503f0fa59d45ce7b6884517d5d.jpg","relation":0},"entries":[{"_id":"57aaa5617a200b1c0abe704c","photo":"http://static1.gotokeep.com/picture/2016/07/10/11/ac0e108f3851c2cce822a61118effac802f3c715_1080x1080.jpg","type":"direct"},{"_id":"57a46ce49bd512b71462f578","photo":"http://static1.gotokeep.com/misc/2016/08/05/18/55968f9830000000.jpg","type":"article"},{"_id":"57a1c4fc15d06cd556b53ddb","photo":"http://static1.gotokeep.com/misc/2016/08/03/19/559409ba60800000.jpg","type":"article"}],"source":"Keep 推荐","reason":null},{"user":{"_id":"57a01d33cceea70c07a4f578","username":"建阔","avatar":"http://static1.gotokeep.com/avatar/2016/08/03/00/039ad66b5607755eae2b86408d5d1effd8954f19.jpg","relation":0},"entries":[{"_id":"57acb6f82ea4c98d4ab91467","photo":"http://static1.gotokeep.com/picture/2016/08/12/01/c18c174496c8bb729579451492d65af6d7da4ccf_281x375.jpg","type":"direct"},{"_id":"57ab224cfde3df32725b2991","photo":"http://static1.gotokeep.com/picture/2016/08/10/20/5a81f02a985c331f4fdcc9593b009e7527115c5b_612x612.jpg","type":"direct"},{"_id":"57a8623e01db931b41846c75","photo":"http://static1.gotokeep.com/picture/2016/08/08/18/5cd044979b04af2544d3ed47ec10e1bbc46a1bde_375x368.jpg","type":"direct"}],"source":"Keep 推荐","reason":null},{"user":{"_id":"5630770d712ffd40cb10160e","username":"秋秋麻麻fighting","avatar":"http://static1.gotokeep.com/avatar/2016/08/03/23/792399e12a6eebc84a2a2895551a9525e733e172.jpg","relation":0},"entries":[{"_id":"57ac7247afc2b24205f5233a","photo":"http://static1.gotokeep.com/picture/2016/08/11/20/b899dfdb0be861909d70f1dd031dff4b232b7e1b_612x612.jpg","type":"direct"},{"_id":"57a5d0da81db502705ac4e1c","photo":"http://static1.gotokeep.com/picture/2016/08/06/19/9a57524822f9e2a3b51dd9f5483c60d4edbb03ce_298x375.jpg","type":"direct"},{"_id":"57a432cfc3b0f72105a551b1","photo":"http://static1.gotokeep.com/picture/2016/08/05/14/b86ee28a49b2697ddeb013087a8cf6efe09235db_612x612.jpg","type":"direct"}],"source":"Keep 推荐","reason":null},{"user":{"_id":"55efec332ec4e889fcc73523","username":"Sicily小姐","avatar":"http://static1.gotokeep.com/avatar/2016/07/28/12/093c6a2dc2d1fc109cd8abd0ab21376c6de59d56.jpg","relation":0},"entries":[{"_id":"57ad5ae68dddee4a4b22b77c","photo":"http://static1.gotokeep.com/picture/2016/08/12/13/165bedd80bb122bf10999d9426ac686e4864fac9_612x612.jpg","type":"normal"},{"_id":"57ac4f7b6d664d08785dedbd","photo":"http://static1.gotokeep.com/picture/2016/08/11/18/51d5b035eed670941872685b1aee6840d09202e8_612x612.jpg","type":"direct"},{"_id":"57ac4db2a57a8e9d77bd4e6f","photo":"http://static1.gotokeep.com/picture/2016/08/11/18/1b4040a617350989fb6d9074c3cb3ef061524ed2_612x612.jpg","type":"direct"}],"source":"Keep 推荐","reason":null},{"user":{"_id":"570fa72d2906982d053a738a","username":"JESS卡卡","avatar":"http://static1.gotokeep.com/avatar/2016/08/04/11/4fa2ca8131fdfd0bd1f2c87e2dfbfb697fb448f1.jpg","relation":0},"entries":[{"_id":"57ad252df596c34b2d486fd7","photo":"http://static1.gotokeep.com/picture/2016/08/12/09/93e3115dc8697340c5d775a28d7cd3d47fe7e5e4_235x320.jpg","type":"normal"},{"_id":"57ac9425508991c01704cae3","photo":"http://static1.gotokeep.com/picture/2016/08/11/23/d71f9a7c7161b2e001a2560f6db243d50131c843_178x320.jpg","type":"normal"},{"_id":"57ac50c8e8a50b4918456174","photo":"http://static1.gotokeep.com/picture/2016/08/11/18/bf7b4310a6d9258c3ffa469a377ef77638732a71_190x320.jpg","type":"normal"}],"source":"Keep 推荐","reason":null},{"user":{"_id":"5748595bba86fd3a05a88ed4","username":"Sunnie-Wang","avatar":"http://static1.gotokeep.com/avatar/2016/08/02/01/b63a239a36990dd1c9daacd287d347ff313694c2.jpg","relation":0},"entries":[{"_id":"57ad4b26bb6544922c315e42","photo":"http://static1.gotokeep.com/picture/2016/08/12/12/9f5e6add231aa81230ee2ad2f853b62d47de91ae_414x308.jpg","type":"direct"},{"_id":"57ac7b6dbfc10a2f18abbd07","photo":"http://static1.gotokeep.com/picture/2016/08/11/21/95609001fc3bd0e01064025c8b67418fbcd3ca06_612x612.jpg","type":"direct"},{"_id":"57abc79669d0b1306d7a8f7d","photo":"http://static1.gotokeep.com/picture/2016/08/11/08/84c63186f3ff733d03faf17101b074ad1a89d4f8_612x612.jpg","type":"direct"}],"source":"Keep 推荐","reason":null},{"user":{"_id":"571d92f9271522c601b6b1e0","username":"馬能能Mee","avatar":"http://static1.gotokeep.com/avatar/2016/08/02/11/1af31562019f12e535b342e64091dcd99710f498.jpg","relation":0},"entries":[{"_id":"57ad36703fceec674a357537","photo":"http://static1.gotokeep.com/picture/2016/08/12/11/8f953e16c851b0984ee245f2ac7908dc1b2e51f1_279x375.jpg","type":"direct"},{"_id":"57ac58e2faf694310556040e","photo":"http://static1.gotokeep.com/picture/2016/08/11/19/873f621893c2493cb6a392829f1892724103e27e_612x612.jpg","type":"direct"},{"_id":"57ab1a963b508b4e06cc88b1","photo":"http://static1.gotokeep.com/picture/2016/08/10/20/658d36aa43524c020d88325113c8330ccdd52c7e_375x346.jpg","type":"direct"}],"source":"Keep 推荐","reason":null},{"user":{"_id":"57a88c899197691e05e061a8","username":"马小鱼Maggie","avatar":"http://wx.qlogo.cn/mmopen/Q3auHgzwzM6n0NImtjcxnb4MMyyblZ7p3JoISytx6aPoicQAq21DibCMRPRsyl8Miahqt80RqbXt9Saob9mWYAvdOxWoUz8RgqB7ibIaGNXSPf8/0","relation":0},"entries":[{"_id":"57ad69cc21f3a44e2c83810d","photo":"http://static1.gotokeep.com/picture/2016/08/12/14/7a0167e377b8b080d80d9f2c8bb8acee77dbd8e9_612x612.jpg","type":"direct"},{"_id":"57abd1760eced2e844330033","photo":"http://static1.gotokeep.com/picture/2016/08/11/09/c3cab5c59b6dc470cabb09457b00d6eea123e1a8_612x612.jpg","type":"direct"},{"_id":"57aae8cb0ea7531072a6655f","photo":"http://static1.gotokeep.com/picture/2016/08/10/16/3b69e8f9e5aaa263f9aeb2bc2f4090676c184cb1_612x612.jpg","type":"direct"}],"source":"Keep 推荐","reason":null},{"user":{"_id":"5575d6b9f3db0dc1050ac0df","username":"马小也野","avatar":"http://static1.gotokeep.com/avatar/2016/07/27/11/17a2d326dbccea9af52d96f60214fadd95c743ca.jpg","relation":0},"entries":[{"_id":"57aab10b83762b640a0a59ff","photo":"http://static1.gotokeep.com/picture/2016/08/10/12/7e7f12ecc05f570d333cc265d5a819b3b2abbe0a_612x612.jpg","type":"direct"},{"_id":"57a73bc510df311f059dcd1b","photo":"http://static1.gotokeep.com/picture/2016/08/07/21/faacb9136ede2ccc0b743d367d13cd4155f5d97e_612x612.jpg","type":"direct"},{"_id":"57a6996ff4c7baab14c566a0","photo":"http://static1.gotokeep.com/picture/2016/08/07/10/4487a26e17a7db84775d8828e021cff9e0d1520d_612x612.jpg","type":"direct"}],"source":"Keep 推荐","reason":null},{"user":{"_id":"575f5f17fbbe7fa21deddee9","username":"傻吧我","avatar":"http://static1.gotokeep.com/avatar/2016/06/22/09/e8d41b62d181c45207b31eab98b24bce46a421ce.jpg","relation":0},"entries":[{"_id":"57ac18f610cadaee71466ba7","photo":"http://static1.gotokeep.com/picture/2016/08/11/14/462a8a790d342708033adebae0cf75df987d8e92_375x279.jpg","type":"normal"},{"_id":"57aaff828be0317406d8e0ea","photo":"http://static1.gotokeep.com/picture/2016/08/10/18/7546048c1b604ee3e63350f67e10155e2da402a6_612x612.jpg","type":"normal"},{"_id":"57a9d49246b53a2305e1c3be","photo":"http://static1.gotokeep.com/picture/2016/08/09/21/21009d3f9aa9c5bd0b9d42145d000dbef5149d6d_328x375.jpg","type":"direct"}],"source":"最近 7 篇精选动态","reason":null},{"user":{"_id":"576945f6d255f64b05262746","username":"HF健身_Ryan","avatar":"http://static1.gotokeep.com/avatar/2016/07/06/20/42ebd8c957357a852c1c87c17554edae3fd7fd5c.jpg","relation":0},"entries":[{"_id":"57ad24ffbb6544922c30a9b3","photo":"http://static1.gotokeep.com/picture/2016/08/12/09/1b097c52503512366e0d54d760636fef6b4207d6_612x612.jpg","type":"direct"},{"_id":"57abd7f8997d19681f2e0c46","photo":"http://static1.gotokeep.com/picture/2016/08/11/09/7ecee0b7e4abe744098b316ecffed3b304af8913_612x612.jpg","type":"direct"},{"_id":"57aa89aa4eb0994b3f5c4b45","photo":"http://static1.gotokeep.com/picture/2016/08/10/09/c7e100ea86c6f1f3db129c58b42b590a5bc55724_612x612.jpg","type":"direct"}],"source":"最近 5 篇精选动态","reason":null},{"user":{"_id":"56e5fd60e10e55ed62ad8ff0","username":"why小宇宙","avatar":"http://static1.gotokeep.com/avatar/2016/03/20/17/fd8bf25650e3af87758c034678a6001af9f4f0b9.jpg","relation":0},"entries":[{"_id":"57ad4b90bb6544922c315f43","photo":"http://static1.gotokeep.com/picture/2016/07/12/12/d98f7f5f803fc7eb5bc9a61e62051a0951b8baed_1080x1029.jpg","type":"direct"},{"_id":"57ac6a40d60580240592aead","photo":"http://static1.gotokeep.com/picture/2016/07/11/20/048e5b33b92161b0e594e2d03a238c660412ba59_1080x1079.jpg","type":"direct"},{"_id":"57ac085cad7cc67c7270da85","photo":"http://static1.gotokeep.com/picture/2016/07/11/13/20d52e8d1d66ca6cd58af23749c4dbf0ec03393f_672x1079.jpg","type":"direct"}],"source":"最近 6 篇精选动态","reason":null},{"user":{"_id":"575449ba9164ae3405811ef0","username":"Jessica金卡卡","avatar":"http://static1.gotokeep.com/avatar/2016/06/05/23/619c1de446dcb7f7cc9410e4e2dcde41ed8644e6.jpg","relation":0},"entries":[{"_id":"57ac8659cf571a3b0547631d","photo":"http://static1.gotokeep.com/picture/2016/08/11/23/2de2ffc517364057dea430f0b499e7bfec700416_329x375.jpg","type":"direct"},{"_id":"57aab967272d3140588a849a","photo":"http://static1.gotokeep.com/picture/2016/08/10/13/f37f071f2b4f2cbefc2d8e8860f0e958aa78e7d4_208x375.jpg","type":"direct"},{"_id":"57a88eb8760db13505ceb6b9","photo":"http://static1.gotokeep.com/picture/2016/08/08/21/330d05cd1934ee12e38d9322e3891a395606d7b2_612x612.jpg","type":"direct"}],"source":"最近 9 篇精选动态","reason":null},{"user":{"_id":"56cfe665af1acaf20330c4f7","username":"语姐姐","avatar":"http://static1.gotokeep.com/avatar/2016/06/08/12/3dd7e86c18241cd140a9e524fef7b1dbde9c841d.jpg","relation":0},"entries":[{"_id":"57ad25376ec5e2ae17976103","photo":"http://static1.gotokeep.com/picture/2016/08/12/09/3b72e8060d9699e03422c7d8698700b7d8e61b96_612x612.jpg","type":"direct"},{"_id":"57ac232cbfc10a2f18a87c5e","photo":"http://static1.gotokeep.com/picture/2016/08/11/15/7a1832fa618afcfc00b4b7b237d64073a4dae886_612x612.jpg","type":"direct"},{"_id":"57ab0994ff0ef76b0677ff67","photo":"http://static1.gotokeep.com/picture/2016/08/10/19/29e9ce9d0cec2f8879637004cd6159edc92b18a0_612x612.jpg","type":"normal"}],"source":"最近 15 篇精选动态","reason":null},{"user":{"_id":"578c356edffdb331058f5a61","username":"Hello_Lilian","avatar":"http://static1.gotokeep.com/avatar/2016/07/21/16/d2df40ee3deb859d474a9fefd4d9537b340ab01d.jpg","relation":0},"entries":[{"_id":"57ad414ce623fb5c18b054d1","photo":"http://static1.gotokeep.com/picture/2016/08/12/11/a39cec486321858eb703587f2fc8a5ca5bef276a_266x375.jpg","type":"normal"},{"_id":"57ad2d362ea4c98d4aba1b7f","photo":"http://static1.gotokeep.com/picture/2016/08/12/09/fd4f89a84262eadf3f5d307422ad9c7bcd434069_612x612.jpg","type":"run"},{"_id":"57ac8e90a57a8e9d77c02804","photo":"http://static1.gotokeep.com/picture/2016/08/11/22/a70c1cb5c9cab2f000d5239743a1a1204d71ec3d_279x375.jpg","type":"normal"}],"source":"最近 6 篇精选动态","reason":null}]
+     * errorCode : 0
+     * now : 2016-08-12T08:03:54.190Z
+     * version : 1.0.0
+     */
+
+    private boolean ok;
+    private int errorCode;
+    private String now;
+    private String version;
+    /**
+     * user : {"_id":"54bf30dd9eb5cd2c4c90be46","username":"Keep","avatar":"http://static1.gotokeep.com/avatar/2016/01/04/15/123ef56eda66391ec83a8a0290fa813bc248c3c8.jpg","relation":0}
+     * entries : [{"_id":"57a83380a1987a1f705f674f","photo":"http://static1.gotokeep.com/misc/2016/08/09/10/559b4f7ce6c00000.jpg","type":"article"},{"_id":"579e9a3023cf904405062a42","photo":"http://static1.gotokeep.com/picture/2016/08/01/08/732ed62b28b5c6d4d62faaacdd5c3dfe606135ed_612x612.jpg","type":"direct"},{"_id":"5797504b7e7db1261d8d0864","photo":"http://static1.gotokeep.com/misc/2016/07/26/19/5589bfb3b3400000.jpg","type":"article"}]
+     * source : Keep 推荐
+     * reason : null
+     */
+
+    private List<DataBean> data;
+
+    public boolean isOk() {
+        return ok;
+    }
+
+    public void setOk(boolean ok) {
+        this.ok = ok;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getNow() {
+        return now;
+    }
+
+    public void setNow(String now) {
+        this.now = now;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public List<DataBean> getData() {
+        return data;
+    }
+
+    public void setData(List<DataBean> data) {
+        this.data = data;
+    }
+
+    public static class DataBean {
+        /**
+         * _id : 54bf30dd9eb5cd2c4c90be46
+         * username : Keep
+         * avatar : http://static1.gotokeep.com/avatar/2016/01/04/15/123ef56eda66391ec83a8a0290fa813bc248c3c8.jpg
+         * relation : 0
+         */
+
+        private UserBean user;
+        private String source;
+        private Object reason;
+        /**
+         * _id : 57a83380a1987a1f705f674f
+         * photo : http://static1.gotokeep.com/misc/2016/08/09/10/559b4f7ce6c00000.jpg
+         * type : article
+         */
+
+        private List<EntriesBean> entries;
+
+        public UserBean getUser() {
+            return user;
+        }
+
+        public void setUser(UserBean user) {
+            this.user = user;
+        }
+
+        public String getSource() {
+            return source;
+        }
+
+        public void setSource(String source) {
+            this.source = source;
+        }
+
+        public Object getReason() {
+            return reason;
+        }
+
+        public void setReason(Object reason) {
+            this.reason = reason;
+        }
+
+        public List<EntriesBean> getEntries() {
+            return entries;
+        }
+
+        public void setEntries(List<EntriesBean> entries) {
+            this.entries = entries;
+        }
+
+        public static class UserBean {
+            private String _id;
+            private String username;
+            private String avatar;
+            private int relation;
+
+            public String get_id() {
+                return _id;
+            }
+
+            public void set_id(String _id) {
+                this._id = _id;
+            }
+
+            public String getUsername() {
+                return username;
+            }
+
+            public void setUsername(String username) {
+                this.username = username;
+            }
+
+            public String getAvatar() {
+                return avatar;
+            }
+
+            public void setAvatar(String avatar) {
+                this.avatar = avatar;
+            }
+
+            public int getRelation() {
+                return relation;
+            }
+
+            public void setRelation(int relation) {
+                this.relation = relation;
+            }
+        }
+
+        public static class EntriesBean {
+            private String _id;
+            private String photo;
+            private String type;
+
+            public String get_id() {
+                return _id;
+            }
+
+            public void set_id(String _id) {
+                this._id = _id;
+            }
+
+            public String getPhoto() {
+                return photo;
+            }
+
+            public void setPhoto(String photo) {
+                this.photo = photo;
+            }
+
+            public String getType() {
+                return type;
+            }
+
+            public void setType(String type) {
+                this.type = type;
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/dllo/keepproject/ui/adapter/FindRecommendRvAdapter.java
@@ -0,0 +1,63 @@
+package com.example.dllo.keepproject.ui.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.dllo.keepproject.R;
+import com.example.dllo.keepproject.model.bean.FindFmrecommendBean;
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by dllo on 16/8/12.
+ */
+public class FindRecommendRvAdapter extends RecyclerView.Adapter<FindRecommendRvAdapter.RecommendRvHolder>{
+    private FindFmrecommendBean datas;
+    private Context context;
+
+    public void setDatas(FindFmrecommendBean datas) {
+        this.datas = datas;
+        notifyDataSetChanged();
+    }
+
+    public FindRecommendRvAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public RecommendRvHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        RecommendRvHolder recommendRvHolder = null;
+        View view = LayoutInflater.from(context).inflate(R.layout.item_fragment_find_recommend_rv,parent,false);
+        recommendRvHolder = new RecommendRvHolder(view);
+        return recommendRvHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecommendRvHolder holder, int position) {
+        holder.usernameTv.setText(datas.getData().get(position).getUser().getUsername());
+        holder.sourceTv.setText(datas.getData().get(position).getSource());
+        Picasso.with(context).load(datas.getData().get(position).getUser().getAvatar()).into(holder.avatarIv);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return datas != null ?datas.getData().size():0;
+    }
+
+    class RecommendRvHolder extends RecyclerView.ViewHolder{
+        TextView usernameTv;
+        TextView sourceTv;
+        ImageView avatarIv;
+        public RecommendRvHolder(View itemView) {
+            super(itemView);
+            usernameTv = (TextView) itemView.findViewById(R.id.find_fragment_recommend_username);
+            sourceTv = (TextView) itemView.findViewById(R.id.find_fragment_recommend_source);
+            avatarIv = (ImageView) itemView.findViewById(R.id.find_fragment_recommend_avatarIv);
+        }
+    }
+}
