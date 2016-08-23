93a6453167b991fefdef1499472a546b91e3b7e1
xuanyuZhou <254075613@qq.com>
2016-08-23 15:03:11 +0800

    注册界面布局

---
+++ app/src/main/java/com/example/dllo/keepproject/ui/activity/RegisterActivity.java
@@ -0,0 +1,29 @@
+package com.example.dllo.keepproject.ui.activity;
+
+import com.example.dllo.keepproject.R;
+
+/**
+ * Created by dllo on 16/8/23.
+ * 注册界面activity
+ */
+public class RegisterActivity extends AbsBaseActivity{
+    @Override
+    protected int setLayout() {
+        return R.layout.activity_register;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initListeners() {
+
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
+}
